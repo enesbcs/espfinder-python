14bc3a736d4a677cbf32a0c94092b4f9e2944d1d
enesbcs <31168072+enesbcs@users.noreply.github.com>
2018-04-09 20:47:43 +0200

    First upload

---
+++ ef_net.py
@@ -0,0 +1,401 @@
+#! /usr/bin/env python
+#
+# Network functions for ESPFinder
+#
+from subprocess import getoutput
+import urllib.request
+import json
+import re
+import os
+import itertools
+from contextlib import closing
+import socket
+
+macesp = [
+'18-FE-34',
+'24-0A-C4',
+'24-B2-DE',
+'2C-3A-E8',
+'30-AE-A4',
+'54-5A-A6',
+'5C-CF-7F',
+'60-01-94',
+'68-C6-3A',
+'84-0D-8E',
+'84-F3-EB',
+'90-97-D5',
+'A0-20-A6',
+'A4-7B-9D',
+'AC-D0-74',
+'B4-E6-2D',
+'BC-DD-C2',
+'CC-50-E3',
+'D8-A0-1D',
+'DC-4F-22',
+'EC-FA-BC']
+
+
+def parseTable(html):
+    #Each "row" of the HTML table will be a list, and the items
+    #in that list will be the TD data items.
+    ourTable = []
+
+    #We keep these set to NONE when not actively building a
+    #row of data or a data item.
+    ourTD = None    #Stores one table data item
+    ourTR = None    #List to store each of the TD items in.
+
+
+    #State we keep track of
+    inTable = False
+    inTR = False
+    inTD = False
+
+    #Start looking for a start tag at the beginning!
+    tagStart = html.find("<", 0)
+
+    while( tagStart != -1):
+        tagEnd = html.find(">", tagStart)
+
+        if tagEnd == -1:    #We are done, return the data!
+            return ourTable
+
+        tagText = html[tagStart+1:tagEnd]
+
+        #only look at the text immediately following the <
+        tagList = tagText.split()
+        tag = tagList[0]
+        tag = tag.lower()
+
+        #Watch out for TABLE (start/stop) tags!
+        if tag == "table":      #We entered the table!
+            inTable = True
+        if tag == "/table":     #We exited a table.
+            inTable = False
+
+        #Detect/Handle Table Rows (TR's)
+        if tag == "tr":
+            inTR = True
+            ourTR = []      #Started a new Table Row!
+
+        #If we are at the end of a row, add the data we collected
+        #so far to the main list of table data.
+        if tag == "/tr":
+            inTR = False
+            ourTable.append(ourTR)
+            ourTR = None
+
+        #We are starting a Data item!
+        if tag== "td":
+            inTD = True
+            ourTD = ""      #Start with an empty item!
+
+        #We are ending a data item!
+        if tag == "/td":
+            inTD = False
+            if ourTD != None and ourTR != None:
+                cleanedTD = ourTD.strip()   #Remove extra spaces
+                ourTR.append( ourTD.strip() )
+            ourTD = None
+
+
+        #Look for the NEXT start tag. Anything between the current
+        #end tag and the next Start Tag is potential data!
+        tagStart = html.find("<", tagEnd+1)
+
+        #If we are in a Table, and in a Row and also in a TD,
+        # Save anything that's not a tag! (between tags)
+        #
+        #Note that this may happen multiple times if the table
+        #data has tags inside of it!
+        #e.g. <td>some <b>bold</b> text</td>
+        #
+        #Because of this, we need to be sure to put a space between each
+        #item that may have tags separating them. We remove any extra
+        #spaces (above) before we append the ourTD data to the ourTR list.
+        if inTable and inTR and inTD:
+            ourTD = ourTD + html[tagEnd+1:tagStart] + " "
+            #print("td:", ourTD)   #for debugging
+
+
+    #If we end the while loop looking for the next start tag, we
+    #are done, return ourTable of data.
+    return(ourTable)
+
+
+def get_ip():
+    f = os.popen('ifconfig')
+    for iface in [' '.join(i) for i in iter(lambda: list(itertools.takewhile(lambda l: not l.isspace(),f)), [])]:
+        if re.findall('^(eth|wlan)[0-9]',iface) and re.findall('RUNNING',iface):
+            ip = re.findall('(?<=inet\saddr:)[0-9\.]+',iface)
+            if ip:
+                return ip[0]
+    return False
+
+def check_port(host, port):
+    opened = False
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        sock.settimeout(2)
+        if sock.connect_ex((host, port)) == 0:
+            #print("Port ",port," is open")
+            opened = True
+    return opened
+
+def getMACfromIP(ipaddr):
+  resarr = []
+  line = getoutput("arp -a %s 2> /dev/null" % ipaddr)
+  try:
+   mac = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", line).groups()[0]
+   resarr.append(mac.strip())
+  except:
+   resarr.append("")
+  strname = line.split(' ')[0]
+  resarr.append(strname.strip())
+  return resarr
+
+def check80(purl):
+ tipus = "Unknown"
+ rescode = 0
+ try:
+  content = urllib.request.urlopen("http://"+purl, None, 2)
+ except:
+  rescode = -1
+  if check_espurna(purl):
+   tipus = "ESPurna"     
+ if rescode == 0: 
+  try:
+   rescode = content.getcode()
+  except:
+   rescode = -1
+  if (rescode == 200):
+   tipus2 = content.info()['Server']
+   retdata = content.read()
+   if str(retdata).find("Sonoff-Tasmota")>-1:
+    tipus = "Tasmota"
+   elif str(retdata).find("Powered by www.letscontrolit.com")>-1: 
+    tipus = "ESPEasy"
+   elif str(retdata).find("/cgi-bin/luci")>-1:
+    tipus = "OpenWRT"
+   elif tipus2 != "" and tipus2 != None:
+    tipus = tipus2
+  else:
+   tipus = "HTTP Err:"+str(rescode)
+ return tipus
+ 
+def get_tasmota(purl):
+    
+#0:IP, 1:MAC, 
+#3:FriendlyName, 2:Tasmota FW_Ver + Core_Ver, 4:Uptime, 5:ProgramSize/FlashSize, 6: Heap, 7:RSSI, (%), 8:Vcc
+ resarr = ['','','','','','','','','']
+ rescode = 0
+ try:
+  content = urllib.request.urlopen("http://"+purl+"/cm?cmnd=status", None, 2)
+ except:
+  rescode = -1
+ if rescode == 0: 
+  try:
+   rescode = content.getcode()
+  except:
+   rescode = -1
+  if (rescode == 200):
+   retdata = content.read()
+   msg2 = retdata.decode('utf-8')
+   if ('{' in msg2):
+    list = []
+    try:
+     list = json.loads(msg2)
+    except Exception as e:
+     print("JSON decode error:",e,"'",msg2,"'")
+     list = []
+    if (list):
+     if list['Status']:
+      resarr[3] = str(list['Status']['FriendlyName'])
+ rescode = 0
+ try:
+  content = urllib.request.urlopen("http://"+purl+"/cm?cmnd=status%202", None, 2)
+ except:
+  rescode = -1
+ if rescode == 0: 
+  try:
+   rescode = content.getcode()
+  except:
+   rescode = -1
+  if (rescode == 200):
+   retdata = content.read()
+   msg2 = retdata.decode('utf-8')
+   if ('{' in msg2):
+    list = []
+    try:
+     list = json.loads(msg2)
+    except Exception as e:
+     print("JSON decode error:",e,"'",msg2,"'")
+     list = []
+    if (list):
+     if list['StatusFWR']:
+      resarr[2] = "Tasmota "+str(list['StatusFWR']['Version']) + " (core " +str(list['StatusFWR']['Core']) + ")"
+ rescode = 0
+ try:
+  content = urllib.request.urlopen("http://"+purl+"/cm?cmnd=status%204", None, 2)
+ except:
+  rescode = -1
+ if rescode == 0: 
+  try:
+   rescode = content.getcode()
+  except:
+   rescode = -1
+  if (rescode == 200):
+   retdata = content.read()
+   msg2 = retdata.decode('utf-8')
+   if ('{' in msg2):
+    list = []
+    try:
+     list = json.loads(msg2)
+    except Exception as e:
+     print("JSON decode error:",e,"'",msg2,"'")
+     list = []
+    if (list):
+     if list['StatusMEM']:
+      resarr[5] = str(list['StatusMEM']['ProgramSize']) + "kB /" + str(list['StatusMEM']['FlashSize'])+" kB"
+      resarr[6] = str(list['StatusMEM']['Heap'])+" kB"
+ rescode = 0
+ try:
+  content = urllib.request.urlopen("http://"+purl+"/cm?cmnd=status%2011", None, 2)
+ except:
+  rescode = -1
+ if rescode == 0: 
+  try:
+   rescode = content.getcode()
+  except:
+   rescode = -1
+  if (rescode == 200):
+   retdata = content.read()
+   msg2 = retdata.decode('utf-8')
+   if ('{' in msg2):
+    list = []
+    try:
+     list = json.loads(msg2)
+    except Exception as e:
+     print("JSON decode error:",e,"'",msg2,"'")
+     list = []
+    if (list):
+     if list['StatusSTS']:
+      resarr[4] = str(list['StatusSTS']['Uptime'])
+      if len(resarr[4]) < 8:
+        resarr[4] += "h"
+      resarr[8] = str(list['StatusSTS']['Vcc'])+"V"
+      resarr[7] = str(list['StatusSTS']['Wifi']['RSSI'])+"%"
+ return resarr
+
+def get_espeasy(purl):
+#0:IP, 1:MAC, 
+#3:Unit num, 2:ESPEasy build + Git build, 4:Uptime, 6: Free RAM,
+#Core_Ver into 2, 5:ProgramSize/FlashSize, 7:RSSI, (%)
+ resarr = ['','','','','','','','','']
+ rescode = 0
+ try:
+  content = urllib.request.urlopen("http://"+purl+"/json", None, 2)
+ except:
+  rescode = -1
+ if rescode == 0: 
+  try:
+   rescode = content.getcode()
+  except:
+   rescode = -1
+  if (rescode == 200):
+   retdata = content.read()
+   msg2 = retdata.decode('utf-8')
+   if ('{' in msg2):
+    list = []
+    try:
+     list = json.loads(msg2)
+    except Exception as e:
+     print("JSON decode error:",e,"'",msg2,"'")
+     list = []
+    if (list):
+     if list['System']:
+      try:   
+       resarr[2] = "ESPEasy "+str(list['System']['Build'])+" "+str(list['System']['Git Build'])
+       resarr[3] = str(list['System']['Unit'])
+       upmin = int(list['System']['Uptime'])
+       resarr[4] = str(round(upmin/60,2))+"h"
+       heapfree = list['System']['Free RAM']
+       resarr[6] = str(round(heapfree/1024,2))+" kB"
+       wifistren = list['WiFi']['RSSI']
+       if wifistren != "" or int(wifistren) < 0:
+        wifistren = 2 * (int(wifistren) + 100)
+        if wifistren > 100:
+         wifistren = 100
+        resarr[7] = str(wifistren) + "%"
+
+      except:
+       pass   
+ rescode = 0
+ try:
+  content = urllib.request.urlopen("http://"+purl+"/sysinfo", None, 2)
+ except:
+  rescode = -1
+ if rescode == 0: 
+  try:
+   rescode = content.getcode()
+  except:
+   rescode = -1
+  if (rescode == 200):
+   readinfos = str(content.read())
+   readinfos2 = readinfos.replace("<TR><TD>","</TD></TR><TR><TD>") # correct missing html markers
+   readinfos2 = readinfos2.replace("</TD></TR></TD></TR><TR><TD>","</TD></TR><TR><TD>")
+   readinfos2 = readinfos2.replace("<TD>","</TD><TD>")
+   readinfos2 = readinfos2.replace("<TR></TD>","<TR>")
+   readinfos2 = readinfos2.replace("</TD></TD><TD>","</TD><TD>")
+   readinfos2 = readinfos2.replace("</table>","</TD></TR></TABLE>")   
+   dataTable = parseTable(readinfos2)
+   for item in dataTable:
+    tarr = str(item)
+    if (tarr.find("Wifi")>0 and tarr.find("802.")>0) or tarr.find("Wifi RSSI")>0:
+        try:
+         wifistren = int(re.findall(r'-\d\d',item[1])[0])
+        except:
+         wifistren = 0
+        if wifistren < 0:
+         wifistren = 2 * (wifistren + 100)
+        if wifistren > 100:
+         wifistren = 100
+        resarr[7] = str(wifistren) + "%"
+    
+    if (tarr.find("Build")>0 and tarr.find("core")>0) or tarr.find("Build:")>0:
+       resarr[2] = "ESPEasy " + item[1]
+        
+    if tarr.find("Flash Chip Real Size")>0 or tarr.find("Flash Size:")>0:
+       resarr[5] = item[1]
+            
+    if tarr.find("Flash IDE mode")>0:
+       resarr[5] += " ("+item[1]+")"
+
+    if tarr.find("Core Version:")>0:
+       resarr[2] += " "+item[1]
+                
+    if tarr.find("Sketch Size")>0:
+       resarr[5] = re.findall(r'\d\d\d kB',item[1])[0] + "/" + resarr[5]
+        
+ return resarr
+
+def check_espurna(purl): # 23 & 80 open
+ wildguess = 0
+ if check_port(purl, 80):
+  try:
+   content = urllib.request.urlopen("http://"+purl+"/index.html", None, 2)
+  except Exception as e:
+   if str(e).find("Error 401:")>0:
+    wildguess += 1
+ if check_port(purl, 23):
+    wildguess += 1
+ return (wildguess==2)   
+
+def checkMACManuf(macaddr):
+ retstr = ""   
+ normmac = str(macaddr).upper().replace(":","-").strip()
+ normmac = normmac[:8]
+ if normmac in macesp:
+  retstr = "Espressif"
+ elif normmac == "B8-27-EB":
+  retstr = "Raspberry"
+ return retstr 
---
+++ espfinder.py
@@ -0,0 +1,189 @@
+#! /usr/bin/env python
+#
+# Pings for active host on *this* sub network.
+# The current sub network address and
+# the host numbers 1-254 will be pinged
+#
+
+from subprocess import getoutput
+from multiprocessing import Process, Array
+from sys import exit, argv
+import ctypes
+import time
+import signal
+from ef_net import *
+import tkinter# sudo apt-get install python3-tk 
+from tkinter import ttk
+
+PROG_NAME= "ESP Finder"
+PROG_VER="0.1b"
+
+# ---------- MULTIPROCESSING PART
+
+shared_array = Array(ctypes.c_ubyte,255)  # multiprocessing ping results
+
+class PING_SWEEP(object):
+
+  def __init__(self,callback,ownip):
+    self.callback = callback
+    self.ownip = ownip
+    self.ping_sweeper.__init__(self)
+    self.ping_sweeper()
+
+  def pinger(self, host_num, resarray):
+    """thread pinger function"""
+    hostadrr = self.ownip.split('.')[:-1]
+    hostadrr = '.'.join(hostadrr) + '.' + repr(host_num)
+    try:
+     line = getoutput("ping -n -c 1 -W 0.2 %s 2> /dev/null" % hostadrr)
+    except:
+     line = ""
+     exit(0)
+#    self.callback(1,0)
+    if line.find(hostadrr) and line.find("bytes from") > -1:  # Host Active
+#        self.callback(0,hostadrr)
+        resarray[host_num] = host_num
+    else:
+        exit(0)
+
+  def ping_sweeper(self):
+    global shared_array
+    self.maxnum = 255
+    procarr = []
+    for host_num in range(1, self.maxnum):
+      ping = Process(target=self.pinger, args=(host_num,shared_array))
+      ping.start()
+      procarr.append(ping.pid)
+    time.sleep(3)
+    for process in procarr: # Cleanup
+     try:
+      os.kill(process,signal.SIGTERM)
+     except:
+      pass 
+
+#  def __del__(self):
+#   self.callback(2,0) 
+
+def cb_stationsearch(func, par1):
+ global ownip, scanlist, UseGUI, progress1
+ if (func == 0) or (func == 1):
+  if UseGUI:
+   progress1.step()
+ elif (func == 2):
+  if UseGUI:
+   progress1["value"] = 254
+
+
+def pingscan():
+ global shared_array, ownip
+ shared_array = Array(ctypes.c_ubyte,255)  # multiprocessing ping results
+ PING_SWEEP(cb_stationsearch,ownip)
+
+def analyzerange():
+ global shared_array, ownip, UseGUI, tree
+ hostaddr = ownip.split('.')[:-1]
+
+ for i in tree.get_children(): # clear tree
+    tree.delete(i)
+
+ for i in range(1,255):
+  if shared_array[i] > 0:
+   hostadr = '.'.join(hostaddr) + '.' + repr(shared_array[i])
+   if (hostadr == ownip):
+    tline = "THIS DEVICE"
+    if UseGUI==False:
+     print(hostadr.ljust(14)+tline)
+    else:
+     tree.insert("","end",text=hostadr, values=(tline))   
+   else:
+    analyzeip(hostadr)
+
+def analyzeip(par1):
+  global UseGUI, tree    
+  # 0:mac,1:netname,2:mactype, 3: ptype
+  attribs = ["","","",""]
+  
+  infoarr = getMACfromIP(par1)
+  if (infoarr[0] != ""):
+    attribs[0] = str(infoarr[0])
+    attribs[1] = str(infoarr[1])
+    attribs[2] = checkMACManuf(infoarr[0])          
+  if (check_port(par1,80)):
+    attribs[3] = check80(par1)
+  if (str(attribs[2]) != str("Espressif")) and (str(attribs[3]) == str("ESPurna")):
+    attribs[3] = "" # its only a wild guess, delete if surely not!
+  tline = attribs[0] + ", "+ attribs[1] + ", "+ attribs[2] +" "+ attribs[3]
+  if UseGUI==False:
+   print(par1.ljust(14) + tline)
+  else:
+   tpid= tree.insert("","end",text=par1.ljust(14),values=(tline))
+  if attribs[3] == "Tasmota":
+   tinfos = get_tasmota(par1)
+   tline = tinfos[2] + ", " + tinfos[3] + ", "+ tinfos[4] +", "+ tinfos[5] +", "+ tinfos[6]+", "+ tinfos[7]+", "+ tinfos[8]
+   if UseGUI==False:
+    print(" -"+tline)
+   else:
+    tree.insert(tpid,"end",values=(tinfos[2],tinfos[3],tinfos[4],tinfos[5],tinfos[6],tinfos[7],tinfos[8]))
+  elif attribs[3] == "ESPEasy":
+   tinfos = get_espeasy(par1)
+   tline = tinfos[2] + ", " + tinfos[3] + ", "+ tinfos[4] +", "+ tinfos[5] +", "+ tinfos[6]+", "+ tinfos[7]
+   if UseGUI==False:       
+    print(" -"+tline)     
+   else:
+    tree.insert(tpid,"end",values=(tinfos[2],tinfos[3],tinfos[4],tinfos[5],tinfos[6],tinfos[7]))
+
+def searchdevices():
+  global UseGUI, progress1    
+  if UseGUI==False:
+   print("Starting pingscan...")
+  pingscan()
+  if UseGUI==False:
+   print("Analyzing online targets...")
+  analyzerange()
+
+UseGUI = True
+  
+if __name__ == '__main__':
+
+  if len(argv)>1:
+   if argv[1] == '-t':
+    UseGUI = False   
+  
+  if UseGUI:
+   window = tkinter.Tk()
+   window.title(PROG_NAME)
+   window.geometry('1000x700')
+   
+  ownip = get_ip()
+  if UseGUI:
+ 
+   tree = ttk.Treeview(window)
+
+   tree["columns"]=("A","B","C","D","E","F","G")
+   tree.column("A", width=250)
+   tree.column("B", width=150)
+   tree.column("C", width=100)
+   tree.column("D", width=120)
+   tree.column("E", width=105)
+   tree.column("F", width=38)
+   tree.column("G", width=50)
+   tree.heading("A", text="Version")
+   tree.heading("B", text="Unit num/name")
+   tree.heading("C", text="Uptime")
+   tree.heading("D", text="Prog/Flash size")
+   tree.heading("E", text="Free RAM")
+   tree.heading("F", text="Wifi")
+   tree.heading("G", text="Vcc")
+
+   tree.pack(expand=True, fill=tkinter.BOTH, side=tkinter.TOP)
+
+   refreshbutton = ttk.Button(window, text='Refresh', command=searchdevices)
+   refreshbutton.pack()
+   refreshbutton.focus_set()
+
+   #draw the window, and start the 'application'
+   window.mainloop()      
+      
+  else:
+   searchdevices()
+   
